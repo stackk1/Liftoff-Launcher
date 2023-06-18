dbe3880e9499dd55609f0ae0ad26b13dea592ade
stackk1 <74647121+stackk1@users.noreply.github.com>
2023-06-17 17:02:08 -0700

    reverse mask fix

---
+++ X/Services/ReverseMask.swift
@@ -0,0 +1,25 @@
+//
+//  ReverseMask.swift
+//  X
+//
+//  Created by Andrew on 2023-06-16.
+//
+
+import SwiftUI
+
+extension View {
+  @inlinable
+  public func reverseMask<Mask: View>(
+    alignment: Alignment = .center,
+    @ViewBuilder _ mask: () -> Mask
+  ) -> some View {
+    self.mask {
+      Rectangle()
+        .overlay(alignment: alignment) {
+          mask()
+            .blendMode(.destinationOut)
+        }
+    }
+  }
+}
+
